f8cfc475eaf66466b6e504a869074f323a2738c7
gyuseok-1316 <ogs1316@gmail.com>
2025-11-07 02:07:56 +0000

    test

--- mysite/scripts/deploy_remote.sh
+++ mysite/scripts/deploy_remote.sh
@@ -32,18 +32,19 @@ python3 -m venv venv || true
 source venv/bin/activate
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r mysite/requirements.txt
 
 # 4) Django 작업
-python3 manage.py makemigrations --noinput || true
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+python3 mysite/manage.py makemigrations --noinput || true
+python3 mysite/manage.py migrate --noinput
+python3 mysitemanage.py collectstatic --noinput
 
 # 5) 기존 runserver 종료
 pkill -f "manage.py runserver" || true
 
 # 6) runserver 재시작
-nohup python3 manage.py runserver 0.0.0.0:8000 > /var/log/mysite_run.log 2>&1 &
+nohup python3 mysite/manage.py runserver 0.0.0.0:8000 > /home/vagrant/projects/mysite/mysite_run.log 2>&1 &
+
 
 echo "DEPLOY_OK $(date)"
 
