9db095e7c11247a67077d3718f45bb937f789ccb
gyuseok-1316 <ogs1316@gmail.com>
2025-11-07 02:17:52 +0000

    test

--- mysite/scripts/deploy_remote.sh
+++ mysite/scripts/deploy_remote.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
 set -euo pipefail
 
-# 실제 배포되는 디렉토리
-PROJECT_DIR=/home/vagrant/projects/
+PROJECT_DIR=/home/vagrant/projects/mysite
+TEMP_DIR=/home/vagrant/projects/__deploy_tmp__
 
-# GitHub repository
 REPO_URL=https://github.com/gyuseok-1316/django_test.git
 BRANCH=main
 
-echo "[INFO] Deploying to: ${PROJECT_DIR}"
+echo "[INFO] Deploying into: ${PROJECT_DIR}"
 
-# 1) 프로젝트 디렉토리 준비
-mkdir -p ${PROJECT_DIR}
+# 0) TEMP DIR 준비
+rm -rf "${TEMP_DIR}"
+mkdir -p "${TEMP_DIR}"
 
-# 2) clone 또는 pull
-if [ -d "${PROJECT_DIR}/.git" ]; then
-    echo "[INFO] Existing Git repo found. Pulling latest changes..."
-    cd "${PROJECT_DIR}"
-    git fetch origin
-    git reset --hard origin/${BRANCH}
-else
-    echo "[INFO] Git repo NOT found. Cloning fresh..."
-    rm -rf "${PROJECT_DIR}"
-    git clone -b ${BRANCH} "${REPO_URL}" "${PROJECT_DIR}"
-    cd "${PROJECT_DIR}"
-fi
+# 1) repo를 TEMP_DIR 에 clone
+git clone -b ${BRANCH} "${REPO_URL}" "${TEMP_DIR}"
 
-# 3) 가상환경 준비
+# 2) repo 안의 mysite/ 내용만 실제 PROJECT_DIR 로 복사
+mkdir -p "${PROJECT_DIR}"
+cp -r ${TEMP_DIR}/mysite/* "${PROJECT_DIR}/"
+
+# TEMP 삭제
+rm -rf "${TEMP_DIR}"
+
+# 3) 가상환경
 cd "${PROJECT_DIR}"
 python3 -m venv venv || true
 source venv/bin/activate
 
 pip install --upgrade pip
-pip install -r mysite/requirements.txt
+pip install -r requirements.txt
 
 # 4) Django 작업
-python3 mysite/manage.py makemigrations --noinput || true
-python3 mysite/manage.py migrate --noinput
-python3 mysitemanage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput || true
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 
 # 5) 기존 runserver 종료
 pkill -f "manage.py runserver" || true
 
 # 6) runserver 재시작
-nohup python3 mysite/manage.py runserver 0.0.0.0:8000 > /home/vagrant/projects/mysite/mysite_run.log 2>&1 &
-
+nohup python3 manage.py runserver 0.0.0.0:8000 \
+    > ${PROJECT_DIR}/mysite_run.log 2>&1 &
 
 echo "DEPLOY_OK $(date)"
-
