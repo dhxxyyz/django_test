8c528bea9195a6a1666b65408b717af046159d05
gyuseok-1316 <ogs1316@gmail.com>
2025-11-07 01:18:26 +0000

    create projects dir

--- mysite/scripts/deploy_remote.sh
+++ mysite/scripts/deploy_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PROJECT_DIR=/home/vagrant/django_git/mysite
+PROJECT_DIR=/home/vagrant/projects
 REPO_URL=https://github.com/gyuseok-1316/django_test.git
 BRANCH=main   # 또는 master
 
